a861bbc510e8c4dbd8bb3fb69e94d8bbcaa11e90
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2023-11-16 21:13:13 -0300

    Inclusão do cliente abstract factory

---
+++ design_patterns/abstract_factory/client.py
@@ -0,0 +1,29 @@
+from rest_framework import views
+from rest_framework.response import Response
+from rest_framework import status
+
+# Dictionary to map payment type to the corresponding factory
+PAYMENT_FACTORIES = {
+    'credit_card': CreditCardPaymentFactory(),
+    # 'paypal': PayPalPaymentFactory(),
+    # 'crypto': CryptocurrencyPaymentFactory(),
+    # Add other factories as needed
+}
+
+class PaymentView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        payment_type = request.data.get('payment_type')
+        factory = PAYMENT_FACTORIES.get(payment_type)
+
+        if not factory:
+            return Response({'error': 'Invalid payment type'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Use the factory to create the serializer and processor
+        serializer = factory.create_payment_serializer()
+        processor = factory.create_payment_processor()
+
+        # Validate and process payment
+        serializer.validate_payment(request.data)
+        processor.process_payment(request.data)
+
+        return Response({'message': 'Payment processed successfully'}, status=status.HTTP_200_OK)
